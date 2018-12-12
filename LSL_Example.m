e78627fd1dd6e7d77e3cef1f1ad7bc8a475774c8
Chad C. Williams <cwilliams.uca@gmail.com>
2018-12-11 19:06:32 -0800

    Updated LSL_Example so that it demonstrates using Lab Recorder

--- LSL_Example.m
+++ LSL_Example.m
@@ -1,17 +1,27 @@
-%mex lsl_resolve_byprop.c
-clear all;clc;close all;
+% Written by Chad C. Williams, University of Victoria, 2018.
+% www.chadcwilliams.com
+% Example script on how to connect, record, and send markers of a Muse Headband (2016 Versions)
+% While recording using Lab Recorder
+
+%% Setup environment
+clear all;clc;close all; %Clean environment
+usingMuse = 1; %Toggle the use of Muse or not
 
 %% Initiate LSL Muse connection and recording
-[lib, info, outlet] = LSL_Muse_Initiation; %This initiates the Muse, opens an 'impedance' check, and begins recording.
+[lib, info, outlet] = LSL_Muse_InitiationLR(usingMuse); %This initiates the Muse, opens an 'impedance' check, and begins recording.
+
+%Reminders
+disp('Once you have checked data quality and started recording in Lab Recorder, press enter.');
+pause
 
 %% Experiment
 
 %Send markers at .1 second intervals
 for counter = 1:10
-    disp(num2str(counter));
-    outlet.push_sample({'1'},0); % Pushes the marker to LSL stream
-    WaitSecs(.1);
+    disp(num2str(counter)); %Display iteration of for loop
+    LSL_flipandmark(counter,win,outlet,usingMuse); % Pushes the marker to LSL stream
+    WaitSecs(.1); %Wait .1 seconds
 end
 
 %% Stop Recording and Disconnect Muse
-LSL_Muse_Shutdown; %This closes terimal windows, forcing the connections to disconnect
+LSL_Muse_Shutdown; %This closes terminal windows, forcing the connections to disconnect
